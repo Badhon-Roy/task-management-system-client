93e155e12fa610d18a082ec5fb44ec5cab62164c
Badhon-Roy <roybadhon286@gmail.com>
2023-12-21 13:10:10 +0600

    login and register system successfuly created

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,32 @@
-import { Link, NavLink } from "react-router-dom";
+import {  NavLink, useNavigate } from "react-router-dom";
+import { AuthContext } from "../AuthProvider/AuthProvider";
+import { useContext } from "react";
+import swal from "sweetalert";
 
 const Navbar = () => {
+    const { user , logOut } = useContext(AuthContext)
+    const navigate = useNavigate()
+    const handleSignOut = () => {
+        swal({
+            title: "Are you sure you want to log out?",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        })
+            .then((willDelete) => {
+                if (willDelete) {
+                    logOut()
+                        .then(() => {
+                            swal("Log Out", "successful", "success")
+                            navigate('/')
+
+                        })
+                        .catch(error => {
+                            console.log(error);
+                        })
+                }
+            });
+    }
     return (
         <div className="bg-gray-300">
             <div className="navbar max-w-[1300px] mx-auto">
@@ -36,7 +62,32 @@ const Navbar = () => {
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <Link to="/login" className="btn">Login</Link>
+                    {
+                        user && <div className="flex items-center gap-2">
+                            <div className="text-right">
+                                <h3 className="font-bold">{user?.displayName}</h3>
+                                <p className="text-xs">{user?.email}</p>
+                            </div>
+                            <div className="dropdown dropdown-end">
+                                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                    <div className="w-10 rounded-full">
+                                        <img alt="Tailwind CSS Navbar component" src={user?.photoURL} />
+                                    </div>
+                                </div>
+                                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                                    <li className="text-xl ml-2 font-medium">{user?.displayName}</li>
+                                    <li>
+                                        <a className="justify-between">
+                                            Profile
+                                            <span className="badge">New</span>
+                                        </a>
+                                    </li>
+                                    <li><a>Settings</a></li>
+                                    <li><button onClick={handleSignOut} className="font-bold text-lg">Log out</button></li>
+                                </ul>
+                            </div>
+                        </div>
+                    }
                 </div>
             </div>
         </div>
